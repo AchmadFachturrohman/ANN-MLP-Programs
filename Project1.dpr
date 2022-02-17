445ccf2d77a1d1a0a30038cc43a7763c48c62072
Achmad Fachturrohman <80017569+AchmadFachturrohman@users.noreply.github.com>
2022-02-17 21:55:17 +0700

    Add files via upload

---
+++ Project1.dpr
@@ -0,0 +1,14 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  FMain in 'FMain.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
